3fa91bca158625a2b93d3b2092f9ea5f08721f56
mrnafiz999 <bnafiz84@gmail.com>
2025-03-29 20:31:53 +0600

    Add themeforest-clone/includes/footer.php 1743258715147

---
+++ themeforest-clone/includes/footer.php
@@ -0,0 +1,37 @@
+</div> <!-- Closing container div from header -->
+    <footer class="bg-dark text-white mt-5">
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-md-4">
+                    <h5>About ThemeForest Clone</h5>
+                    <p>A complete PHP clone of ThemeForest marketplace with all core functionalities.</p>
+                </div>
+                <div class="col-md-4">
+                    <h5>Quick Links</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="../public/products.php" class="text-white">All Products</a></li>
+                        <li><a href="../public/login.php" class="text-white">Login</a></li>
+                        <li><a href="../public/register.php" class="text-white">Register</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h5>Connect With Us</h5>
+                    <div class="social-links">
+                        <a href="#" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="text-white me-2"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="text-white me-2"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-center py-3 bg-secondary">
+            &copy; <?php echo date('Y'); ?> ThemeForest Clone. All rights reserved.
+        </div>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Custom JS -->
+    <script src="../assets/js/main.js"></script>
+</body>
+</html>
